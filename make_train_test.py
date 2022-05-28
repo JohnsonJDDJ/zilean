ad937b0a290eeece3fc8274b1182ed4f6b603da5
Johnson Du <zhihao617@berkeley.edu>
2022-05-28 16:32:32 -0700

    rename

--- make_train_test.py
+++ make_train_test.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -15,3 +16,7 @@ data = pd.DataFrame([snap.summary() for snap in snap_list])
 train, test = train_test_split(data, test_size=0.33, random_state=42)
 train.to_csv("data/train.csv")
 test.to_csv("data/test.csv")
+
+def main():
+    # Parse input: choice of frames
+    args = sys.argv[1:]
