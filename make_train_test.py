8ae46d6ccf5f5ca686c68cd21aa7f8b4d6c1bc3c
Johnson Du <zhihao617@berkeley.edu>
2022-05-28 16:34:47 -0700

    main method

--- make_train_test.py
+++ make_train_test.py
@@ -4,19 +4,25 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from zilean.snapshots import SnapShots
 
-# Load cleaned matches
-with open('data/matches_cleaned.json') as f:
-    matches = json.load(f)
-
-# Make DataFrame
-snap_list = [SnapShots(match['timeline'], [8, 12], matchid=match['id']) for match in matches]
-data = pd.DataFrame([snap.summary() for snap in snap_list])
-
-# Split train test and save to disk
-train, test = train_test_split(data, test_size=0.33, random_state=42)
-train.to_csv("data/train.csv")
-test.to_csv("data/test.csv")
 
 def main():
     # Parse input: choice of frames
     args = sys.argv[1:]
+    frames = [int(arg) for arg in args]
+
+    # Load cleaned matches
+    with open('data/matches_cleaned.json') as f:
+        matches = json.load(f)
+
+    # Make DataFrame
+    snap_list = [SnapShots(match['timeline'], [8, 12], matchid=match['id']) for match in matches]
+    data = pd.DataFrame([snap.summary() for snap in snap_list])
+
+    # Split train test and save to disk
+    train, test = train_test_split(data, test_size=0.33, random_state=42)
+    train.to_csv("data/train.csv")
+    test.to_csv("data/test.csv")
+
+
+if __name__ == "__main__":
+    main()
