db258e9fe277baa5d43d7e22bd9ce0ca76fb5a38
Johnson Du <zhihao617@berkeley.edu>
2022-06-08 16:01:21 -0700

    added creep score and porportion option

--- zilean/snapshots.py
+++ zilean/snapshots.py
@@ -4,18 +4,21 @@ from .core import *
 
 class SnapShots:
 
-    def __init__(self, timeline, frames=[8], matchid=None) -> None:
+    def __init__(self, timeline, frames=[8], matchid=None, creep_score=True, porportion=True) -> None:
         self.timeline = timeline
         self.matchid = matchid
         self.frames = frames
         self.win = timeline['info']['frames'][-1]['events'][-1]['winningTeam'] == 100
+        self.creep_score = creep_score
+        self.porportion = porportion
         self.summary_ = None;
         self.frame_independent_summary_ = None;
 
     def summary(self) -> dict:
         """Return summary statistics of all the timeframes of interest as one dictionary."""
         if not self.summary_:
-            self.summary_ = process_timeframe(self.timeline, self.frames, self.matchid)
+            self.summary_ = process_timeframe(self.timeline, frames=self.frames, matchid=self.matchid,
+                                              creep_score=self.creep_score, porportion=self.porportion)
         return self.summary_
 
     def frame_independent_summary(self) -> list:
@@ -26,7 +29,8 @@ class SnapShots:
         if not self.frame_independent_summary_:
             result_list = []
             for frame in self.frames:
-                frame_dic = process_timeframe(self.timeline, [frame], self.matchid)
+                frame_dic = process_timeframe(self.timeline, frames=[frame], matchid=self.matchid,
+                                              creep_score=self.creep_score, porportion=self.porportion)
                 frame_dic['frame'] = frame
                 result_list += [frame_dic]
             self.frame_independent_summary_ = result_list
