1175f56ae79cdacf580145ee02bf5be79e33943c
Johnson Du <zhihao617@berkeley.edu>
2022-06-07 23:28:05 -0700

    added some more dosctrings

--- zilean/snapshots.py
+++ zilean/snapshots.py
@@ -11,9 +11,14 @@ class SnapShots:
         self.win = timeline['info']['frames'][-1]['events'][-1]['winningTeam'] == 100
 
     def summary(self) -> dict:
+        """Return summary statistics of all the timeframes of interest as one dictionary."""
         return process_timeframe(self.timeline, self.frames, self.matchid)
 
     def frame_independent_summary(self) -> list:
+        """
+        Return multiple summaries of statistics, where each summary is only responsible for
+        a single timeframe of interst.
+        """
         result_list = []
         for frame in self.frames:
             frame_dic = process_timeframe(self.timeline, [frame], self.matchid)
@@ -27,7 +32,11 @@ class SnapShots:
         self.watcher = LolWatcher(api_key=key)
         return self.watcher
 
-    def fetch_match_summary(self, region) -> dict:
+    def fetch_match_info(self, region) -> dict:
+        """
+        Fetch the corresponding match info (Riot MatchDto) through lolwatcher.
+        You must have the LolWatcher object fetched before hand.
+        """
         if not self.watcher:
             raise NameError("LolWatcher does not exist. Please connect with Riot API with `.fetch_lolwatcher()` with your api key")
         return self.watcher.match.by_id(region, self.matchid)
