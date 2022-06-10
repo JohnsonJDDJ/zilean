45b27fc5954bb5a161c8e1390592bc49d4aca8a8
Johnson Du <zhihao617@berkeley.edu>
2022-06-10 16:59:47 -0700

    removed lazy load

--- zilean/snapshots.py
+++ zilean/snapshots.py
@@ -27,64 +27,59 @@ class SnapShots:
     - creep_score: Boolean. If True (recommended), then compute the creep score for the players, then
       drop the `minionKilled` and `jungleMinionKilled` feature of the players.
     - porportion: Boolean. If True, then add `goldPorportion` and `xpPorportion` as features to the players.
+    - verbose: Boolean, default False. If True, print out the progress of loading the source data.
     """
 
-    def __init__(self, timelines, frames=[8], creep_score=True, porportion=True) -> None:
+    def __init__(self, timelines, frames=[8], creep_score=True, porportion=True, verbose=False) -> None:
         self.timelines = timelines
         self.frames = frames
         self.creep_score = creep_score
         self.porportion = porportion
-        self.summary_ = None;
-        self.per_frame_summary_ = None;
+        self.summary_ = []
+        self.per_frame_summary_ = []
+
+        # Compute summary_ and per_frame_summary_ 
+        # Load the timelines from source
+        with open(self.timelines) as f:
+            if verbose:
+                print(f"Loading file {self.timelines}. It might take >5 min if file is large.")
+            matches = json.load(f)
+            if verbose:
+                print(f"There is in total {len(matches)} matches successfully loaded.")
+        # Unpack file into dictionaries
+        if verbose:
+            print(f"Unpacking matches into dictionaries.")
+        for match in matches:
+            matchid = match['id']
+            timeline = match['timeline']
+            # Per match summary
+            self.summary_ += [process_timeframe(timeline, frames=self.frames, matchid=matchid,
+                                                creep_score=self.creep_score, porportion=self.porportion)]
+            # Per frame summary
+            for frame in self.frames:
+                frame_dic = process_timeframe(timeline, frames=[frame], matchid=matchid,
+                                                creep_score=self.creep_score, porportion=self.porportion)
+                frame_dic['frame'] = frame
+                self.per_frame_summary_ += [frame_dic]
+        del matches
 
 
-    def summary(self, per_frame=False, verbose=False) -> list:
+    def summary(self, per_frame=False) -> list:
         """
         Return the summary for all the matches (Riot MatchTimelineDtos). For each match,
-        summary statistics of every time frame of interest is computed. The summary is ready
+        summary statistics of every time frame of interest is returned. The summary is ready
         for further data analysis.
 
         Keyword Arguments:
         
         - per_frame: Boolean. If False (default), each match (Riot MatchTimelineDto) is
         one dictionary. If True, each frame (in minutes) of a match is one dictionary.
-        - verbose: Boolean, default False. If True, print out the progress.
 
         Return:
         
         - A list of dictionaries, ready for further data analysis. Each dictionary is either
         a match or a frame (see `per_frame`). 
         """
-
-        # Compute summary_ and per_frame_summary_ if they are not already cached
-        if (not self.summary_) or (not self.per_frame_summary_):
-            # Load the timelines from source
-            with open(self.timelines) as f:
-                if verbose:
-                    print(f"Loading file {self.timelines}. It might take >5 min if file is large.")
-                matches = json.load(f)
-                if verbose:
-                    print(f"There is in total {len(matches)} matches successfully loaded.")
-
-            if verbose:
-                    print(f"Unpacking matches into dictionaries.")
-            self.summary_ = []
-            self.per_frame_summary_ = []
-
-            for match in matches:
-                matchid = match['id']
-                timeline = match['timeline']
-                # Match summary
-                self.summary_ += [process_timeframe(timeline, frames=self.frames, matchid=matchid,
-                                                    creep_score=self.creep_score, porportion=self.porportion)]
-                # Per frame summary
-                for frame in self.frames:
-                    frame_dic = process_timeframe(timeline, frames=[frame], matchid=matchid,
-                                                  creep_score=self.creep_score, porportion=self.porportion)
-                    frame_dic['frame'] = frame
-                    self.per_frame_summary_ += [frame_dic]
-            del matches
-
         # Return the summary based on `per_frame`
         if per_frame:
             return self.per_frame_summary_
@@ -94,9 +89,6 @@ class SnapShots:
 
     def to_disk(self) -> None:
         """Save the summaries to disk as csv files using pandas.DataFrame.to_csv()"""
-        if not self.summary_ or not self.per_frame_summary_:
-            raise ValueError("Summary statistics are yet to be computed. Please run `summary()` first.")
-
         path = "data/"
         file_name = '_'.join(str(e) for e in self.frames)
 
